f012d4f35f1e2089dfbe1dd659d38145fab32363
Tushar1828 <tushar.sharma0128@gmail.com>
2026-08-25 23:22:37 +0530

    initiated_git_uplaod

---
+++ learning_python/main.py
@@ -0,0 +1,85 @@
+#--name = "TUSHAR SHARMA"
+#price = 37.77
+#age = 25 
+
+#.print(type(name))
+#print(type(price ))
+#print(type(age ))'
+
+#
+#name=input("name:")
+#age=int(input("age:"))
+#price=float(input("price:"))
+#print("My name is",name,"and i am ")
+
+#conditional statement
+#light = input("light color :")
+
+#if(light == "red "):
+ #   print("stop")
+#elif(light == "yellow "):
+#    print("look")
+#elif(light == "green"):
+ #   print("go")
+#else:
+ #   print("light is broken")
+
+#a = float(input("a :"))
+#b = float(input("b :"))
+#c = float(input("c : "))
+#print(a*b*c /100)
+
+#assignment operators
+#num = 10
+#num **= 5 
+#print("num :",num)
+
+#name = input("enter your name: ")
+#age = input ("enter your age :")
+#marks = input("enter your marks:")
+
+#print("welcome", name)
+#print("age=", age )
+#print("marks=", marks )
+
+#marks = int(input("enter student marks:"))
+
+#if(marks >= 90):
+ #   grade = "A"
+#elif(marks >= 80 and marks < 90):
+ #   grade = "B"
+#elif(marks >=70  and marks <80):
+ #   garde = "C"
+#else:
+  #  grade = "D"
+
+ #   print("grade of the student",grade)
+    
+
+#LIST
+#ist= (2, 1, 3)
+#list.append(4)
+#list.sort()
+#list.sort(reverse=True)
+#list.reverse()
+
+#list = ['a','s','v','f']
+#print(list.sort())
+#print(list)
+  
+#SET
+#collection = {1, 2, 3, 4}
+#print(collection)
+#print(type(collection))
+
+#LOOP
+#i = 1
+#while i<= 10 :
+#    print(i)
+#    i += 1
+
+
+#i = 1
+#while i<= 10:
+ #print(i)
+#i -=1
---
+++ learning_python/solution.py
@@ -0,0 +1,68 @@
+#write a program to input 2 no. & print their sum.
+
+#first = input("enter first:")
+#second = input("enter second:")
+
+#print("sum=", first +second)
+
+#wap to input 2 int numbers,a and b . print true if a is greater than or equal to b.
+#a = input("enter first:")
+#b = input("enter second:")
+#print(a>=b)
+
+
+#WAP TO FIND THE GREATEST OF 3 NUMBERS ENTERED BY THE USER .
+
+#a = int(input("enter first number:"))
+#b = int(input("enter second number:"))
+#c = int(input("enter third number:"))
+
+#if ( a>= b and a>=c ):
+ #   print("first number is largest",a)
+#elif(b>=c):
+ #print("second number is largest",b)
+#else:
+ #print("third number is largest",c)
+
+ #WAP TO CHECK IF A NUMBER IS A MULTIPLE OF 7 OR NOT. 
+
+#x = int(input("enter number:"))
+
+#if (x % 9 == 0):
+ #print("multiple of 9")
+#else:
+# print("not a multiple")
+
+#WAP TO CHECK IF A LIST CONTAINS A PALINDROME OF ELEMENTS.
+ 
+#list1 = [1, 3, 6]
+
+#copy_list1 = list1.copy()
+#copy_list1.reverse()
+
+#if (copy_list1 == list1):
+ #   print("palindrome")
+#else:
+ #   print("not palindrome")
+
+#DICTIONARY
+#marks = {}
+
+#x = int(input("enter phy :"))
+#marks.update({"phy" : x})
+
+#x = int(input("enter math :"))
+#marks.update({"math" : x})
+
+#x = int(input("enter chem :"))
+#marks.update({"chem" : x})
+
+#print(marks)
+ 
+#PRINT THE MULTIPLICATION TABLE OF A NUMBER N.
+ 
+
+i = 1
+while i <= 10:
+    print(5*i)   
+    i += 1
