d7b8531fcf83917bbf2291db74b408f1a62e1839
Tushar1828 <tushar.sharma0128@gmail.com>
2026-08-26 22:09:58 +0530

    fun. and range solutions

--- learning_python/Functions.py
+++ learning_python/Functions.py
@@ -1,6 +1,17 @@
-def calc_sum(a , b):
-    return a + b
+#def calc_sum(a , b):
+   # return a + b
 
-sum = calc_sum(100, 490)
-print(sum)
+#sum = calc_sum(100, 490)
+#print(sum)
+
+#AVGERAGE OF 3  NUMBERS
+
+def calc_avg(a, b, c):
+    sum = a + b + c
+    avg = sum / 3
+    print(avg)
+    return avg
+
+calc_avg( 45, 76 , 90)
+
 
