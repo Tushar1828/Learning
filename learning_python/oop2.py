e3228aed46991ac309cb94a1bd14046db27646d2
Tushar1828 <tushar.sharma0128@gmail.com>
2026-08-30 21:39:22 +0530

    basic concepts

--- learning_python/oop2.py
+++ learning_python/oop2.py
@@ -1,10 +1,22 @@
 #DELETE
 
-class students:
-    def __init__(self,name):
-        self.name = name
+# class students:
+#     def __init__(self,name):
+#         self.name = name
 
-s1 = students("tushar")
-print(s1.name)
-del s1.name
-print(s1.name)        
+# s1 = students("tushar")
+# print(s1.name)
+# del s1.name
+# print(s1.name) 
+
+#BASIC CONCEPT
+class persons:
+    __name = "anonymous"
+
+    def __hello(self):
+        print("hello person!")
+    def welcome(self):
+        self.__hello()
+p1 = persons()
+
+print(p1.welcome())
