26f6f9ba10a7727c12532d930678a131308a45d2
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-03 23:46:37 +0530

    pass_len random

--- learning_python/passwordgenerator.py
+++ learning_python/passwordgenerator.py
@@ -4,9 +4,13 @@ import string
 
 pass_len = 8
 charvalues = string.ascii_letters + string.digits + string.punctuation
-password = ""
-for i in range(pass_len):
-    password += random.choice(charvalues)
+
+password = "".join([random.choice(charvalues)for i in range(pass_len)])
+# password = " "
+
+
+# for i in range(pass_len):
+    # password += random.choice(charvalues)
 
 print("your random password is: ",password)
 
