03edced99e43efd6b081b6beeda94eb12101935f
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-04 21:56:32 +0530

    add more items

--- learning_python/miniproject3.py
+++ learning_python/miniproject3.py
@@ -16,4 +16,13 @@ item_1 = input("Enter the name of item you want to order = ")
 if item_1 in menu:
     order_total += menu[item_1]
     print(f"Your item{item_1} has been added to your order")
-    
+
+else:
+    print(f"Ordered item {item_1} is not available yet!")
+
+another_order = input("Do you want to add another item ? (yes/no)")
+if another_order == "Yes":
+    item_2  = input("Enter the name of second item=")
+    if item_2 in menu:
+        order_total += menu[item_2]
+        print(f"Item {item_2} has been added to order")        
