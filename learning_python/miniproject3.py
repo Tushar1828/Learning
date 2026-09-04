b222a404e74ccde817751443599e3a1b9fd826e1
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-04 21:23:17 +0530

    menuwithprice

---
+++ learning_python/miniproject3.py
@@ -0,0 +1,11 @@
+ #restaurnment management system
+menu = {
+    'BURGER' : 80,
+    'PIZZA' : 120,
+    'COFFEE': 70,
+    'PASTA' : 120,
+    'ICE CREAME' : 50,
+}
+#GREET
+print("welcome to PYTHON RESTAURANT")
+print("BURGER : 80\nPIZZA : 120\nCOFFEE: 70\nPASTA : 120\nICE CREAME : 50")
