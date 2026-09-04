d749aa98765837a878ce5da1651c167ba313cdc6
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-04 22:06:40 +0530

    ORDERED

--- learning_python/miniproject3.py
+++ learning_python/miniproject3.py
@@ -25,4 +25,8 @@ if another_order == "Yes":
     item_2  = input("Enter the name of second item=")
     if item_2 in menu:
         order_total += menu[item_2]
-        print(f"Item {item_2} has been added to order")        
+        print(f"Item {item_2} has been added to order")  
+    else:
+        print(f"Order item {item_2} is not avaialable!")
+
+print(f"The total amount of items to pay is {order_total}")                  
