67f7bb4f7d2c68017052f47a80e14ae4ca3e8672
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-04 22:13:04 +0530

    canaddMOREITEMS

--- learning_python/miniproject3.py
+++ learning_python/miniproject3.py
@@ -4,13 +4,15 @@ menu = {
     'PIZZA' : 120,
     'COFFEE': 70,
     'PASTA' : 120,
+    'COFFEE' : 50,
     'ICE CREAME' : 50,
-}
+    'PATTIES' : 50,
+    }
 #GREET
 print("welcome to PYTHON RESTAURANT")
 print("BURGER : 80\nPIZZA : 120\nCOFFEE: 70\nPASTA : 120\nICE CREAME : 50")
 
-order_total = 0
+order_total  = 0
 
 item_1 = input("Enter the name of item you want to order = ")
 if item_1 in menu:
