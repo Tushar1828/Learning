2ffef6c57c6411591f52923e02841419e10259aa
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-05 22:27:57 +0530

    cube/squareroot

--- learning_python/solution5.py
+++ learning_python/solution5.py
@@ -8,7 +8,14 @@ class calculator:
     def square(self):
         print(f"The square is {self.n*self.n}")
 
-    
+    def cube(self):
+        print(f"The cube is {self.n*self.n*self.n}")
+
+    def squareroot(self):
+        print(f"the squareroot is  {self.n**1/2}")
+
 
 a = calculator(5)
-a.square 
+a.square ()
+a.cube()
+a.squareroot()
