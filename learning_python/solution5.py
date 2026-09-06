a77dfe50a4dbfe18a78caa109e9ca3f5da44b0e4
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-06 14:10:11 +0530

    Trainfare/no

--- learning_python/solution5.py
+++ learning_python/solution5.py
@@ -1,26 +1,44 @@
 #WRITE A CLASS "CALCULATOR" CAPABLE OF FINDING SQUARE,CUBE
 # AND SQUARE ROOT OF A NUMBER.
 
-class calculator:
-    def __init__(self, n):
-        self.n = n
+# class calculator:
+#     def __init__(self, n):
+#         self.n = n
 
-    def square(self):
-        print(f"The square is {self.n*self.n}")
+#     def square(self):
+#         print(f"The square is {self.n*self.n}")
 
-    def cube(self):
-        print(f"The cube is {self.n*self.n*self.n}")
+#     def cube(self):
+#         print(f"The cube is {self.n*self.n*self.n}")
 
-    def squareroot(self):
-        print(f"the squareroot is  {self.n**1/2}")
+#     def squareroot(self):
+#         print(f"the squareroot is  {self.n**1/2}")
 
-    @staticmethod
-    def Hey():
-        print("Hey Buddy!")
+#     @staticmethod
+#     def Hey():
+#         print("Hey Buddy!")
     
 
-a = calculator(5)
-a.Hey()
-a.square ()
-a.cube()
-a.squareroot()
+# a = calculator(5)
+# a.Hey()
+# a.square ()
+# a.cube()
+# a.squareroot()
+
+#write a class train which has method to book,
+#get status and get fare inforamtion of train running underinidian railways.
+
+class Train:
+
+    def __init__(self, trainNo):
+        self.tarinNo = trainNo
+    def book(self):
+        pass
+
+    def getstatus(self, trainNo):
+        pass
+
+    def getFare(self):
+        print(f"Ticket fare in train no: {self.trainNo} from {fro}to {to} is
+              {randit(22, 5555)}")
+
