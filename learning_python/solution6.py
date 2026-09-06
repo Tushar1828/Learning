af5d6d022b8baf834efcaa72ba16274352f21f19
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-06 22:00:00 +0530

    vectors

---
+++ learning_python/solution6.py
@@ -0,0 +1,23 @@
+#create a class (2-D vector) and use it to create another 
+#class representing a 3-D vector.
+
+class Twovector:
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+
+    def show(self):
+        print(f"The vector is {self.i}i + {self.j}j")
+
+class Thirdvector(Twovector):
+    def __init__(self, i, j, k):
+        super().__init__(i, j)
+        self.k = k
+
+    def show(self):
+        print(f"The vector is {self.i}i + {self.j}j + {self.k}")  
+
+a = Twovector(1, 2)
+a.show()
+b = Thirdvector(1, 2, 3)
+b.show()         
