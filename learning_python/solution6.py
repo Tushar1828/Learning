1845c4812809890b863ebe85fecba82dc3de9231
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-06 22:27:26 +0530

    employeeincrement

--- learning_python/solution6.py
+++ learning_python/solution6.py
@@ -33,4 +33,11 @@ class Employee:
     def salaryAfterIncrement(self):
         return (self.salary + self.salary * (self.increment/100))
 
-    
+    @salaryAfterIncrement.setter
+    def salaryAfterIncrement(self, salary):
+        self.increment = ((salary/self.salary) -1)*100
+
+
+e = Employee()
+e.salaryAfterIncrement = 690
+print(e.increment)
