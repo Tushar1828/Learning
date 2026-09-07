378a7efe556cb934c6f40319f31332c30e30450a
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-07 22:05:11 +0530

    enumeratenumbers

--- learning_python/solution7.py
+++ learning_python/solution7.py
@@ -1,9 +1,18 @@
 #LIST COMPREHENSION
 
-mylist = [4, 6, 8, 3, 0]
+# mylist = [4, 6, 8, 3, 0]
 
-squaredlist = []
-for item in mylist:
-    squaredlist.append(item*item)
+# squaredlist = []
+# for item in mylist:
+#     squaredlist.append(item*item)
 
-print(squaredlist)
+# print(squaredlist)
+
+#WAP TO PRINT THIRD,FIFTH AND SEVENTH ELEMENT FROM A LIST
+#USING ENUMERATE FUNCTION.
+
+l = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for i, item in enumerate(l):
+    if i== 2 or i == 5 or i == 7:
+        print(item)
