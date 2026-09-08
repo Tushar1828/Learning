18da6c787be15ef88504a58922058df911df2d71
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-08 21:39:54 +0530

    str (table)

--- learning_python/solution7.py
+++ learning_python/solution7.py
@@ -27,9 +27,17 @@
 #WAP TO DISPLAY A/B WHERE A AND B ARE INTEGERS.
 #IF B=0, DISPLAY INFINITE BY HANDLING THE'ZERODIVISIONERROR'.
 
-try:
-    a = int(input("Enter a: "))
-    b = int(input("Enter b: "))
-    print(a/b)
-except ZeroDivisionError as v:
-    print("Infinite")    
+# try:
+#     a = int(input("Enter a: "))
+#     b = int(input("Enter b: "))
+#     print(a/b)
+# except ZeroDivisionError as v:
+#     print("Infinite") 
+# 
+#STORE THE MULTIPLICATION TABLES GENERATED IN PROBLEM 3 IN A FILE NAMED TABLES.TEXT.
+
+n = int(input("Enter a number:"))
+
+table = [n*i for i in range(1, 11)]
+with open ("tables.txt", "a") as f:
+    f.write(str(table) + "\n")
