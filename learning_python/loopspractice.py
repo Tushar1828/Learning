9935318a6b16584a797451dbbc2656b1b0220c34
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-09 21:39:40 +0530

    tablesofanynumber

--- learning_python/loopspractice.py
+++ learning_python/loopspractice.py
@@ -1,11 +1,16 @@
 #WAP TO FIND WHETHER A GIVEN NUMBER IS PRIME OR NOT.
 
+# n = int(input("Enter a number:"))
+
+# for i in range(2 , n):
+#     if (n%i) == 0:
+#         print("Number is not prime")
+#         break
+
+# else:
+#     print("Number is prime")
+
 n = int(input("Enter a number:"))
 
-for i in range(2 , n):
-    if (n%i) == 0:
-        print("Number is not prime")
-        break
-
-else:
-    print("Number is prime")
+for i in range(1,21):
+    print(f"{n} x {i} = {n * i}")
