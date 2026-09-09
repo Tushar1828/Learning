bf91f6fcf325046aca1dbcb119fe482b32cbff1d
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-09 22:02:39 +0530

    patterns

--- learning_python/loopspractice.py
+++ learning_python/loopspractice.py
@@ -10,7 +10,18 @@
 # else:
 #     print("Number is prime")
 
-n = int(input("Enter a number:"))
+# n = int(input("Enter a number:"))
 
-for i in range(1,21):
-    print(f"{n} x {i} = {n * i}")
+# for i in range(1,21):
+#     print(f"{n} x {i} = {n * i}")
+
+n = int(input("Enter the number:"))
+for i in range(1, n+1):
+    if(i ==1 or i ==n):
+        print("*"* n, end="")
+
+    else:
+        print("*" ,end="")
+        print(""* (n-2), end="")
+        print("*", end="")
+    print("")
