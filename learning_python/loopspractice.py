75bb19abc426481b4c348ac3eceb33c8b60f0fa1
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-09 22:18:29 +0530

    reverse

--- learning_python/loopspractice.py
+++ learning_python/loopspractice.py
@@ -15,13 +15,23 @@
 # for i in range(1,21):
 #     print(f"{n} x {i} = {n * i}")
 
-n = int(input("Enter the number:"))
-for i in range(1, n+1):
-    if(i ==1 or i ==n):
-        print("*"* n, end="")
+# n = int(input("Enter the number:"))
+# for i in range(1, n+1):
+#     if(i ==1 or i ==n):
+#         print("*"* n, end="")
 
-    else:
-        print("*" ,end="")
-        print(""* (n-2), end="")
-        print("*", end="")
-    print("")
+#     else:
+#         print("*" ,end="")
+#         print(""* (n-2), end="")
+#         print("*", end="")
+#     print("")
+
+#WAP TO PRINT MULTIPLICATION TABLE OF N USING FOR 
+#LOOPS IN REVERSED ORDER.
+n = int(input("Enter a number:"))
+for i in range(1,21):
+
+    print(f"{n} x {11-i} = {n*(11-i)}")
+
+
+        
