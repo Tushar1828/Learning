1531385213c74329f66c944733393b7bc7d1d533
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-10 17:28:14 +0530

    fahrenheitTocelsius

--- learning_python/Functionpractice.py
+++ learning_python/Functionpractice.py
@@ -1,19 +1,22 @@
 #WAP USING FUNCTIONS TO FIND GREATEST OF THREE NUMBERS.
-a = 1
-b = 2
-c = 3
+# def greatest(a, b, c):
+#     if(a>b and a>c):
+#         return a
+#     elif(b>a and b>c):
+#         return b
+#     elif(c>b and c>a):
+#         return c
 
-def greatest(a, b, c):
-    if(a>b and a>c):
-        return a
-    elif(b>a and b>c):
-        return b
-    elif(c>b and c>a):
-        return c
+# a = 100
+# b = 199
+# c = 299
+# print(greatest(a, b, c))
 
-a = 100
-b = 199
-c = 299
-print(greatest(a, b, c))
+#WAP PROGRAM USING FUCNTION TO COVERT CELSIUS TO FAHRENHEIT.
 
-      
+def f_to_c(f):
+ return 5*(f-32)/9   
+f = int(input("Enter temperature in F:"))
+
+
+print(f"{f_to_c(f)} °C")
