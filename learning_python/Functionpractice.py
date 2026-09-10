8dec6b9f568228c7d03a8ad116d12ab8a498c8e8
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-10 17:13:13 +0530

    GreatestNumber

---
+++ learning_python/Functionpractice.py
@@ -0,0 +1,19 @@
+#WAP USING FUNCTIONS TO FIND GREATEST OF THREE NUMBERS.
+a = 1
+b = 2
+c = 3
+
+def greatest(a, b, c):
+    if(a>b and a>c):
+        return a
+    elif(b>a and b>c):
+        return b
+    elif(c>b and c>a):
+        return c
+
+a = 100
+b = 199
+c = 299
+print(greatest(a, b, c))
+
+      
