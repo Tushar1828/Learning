80081ce4aaf8a8815047bb489f62c950a7f08fda
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-11 21:29:45 +0530

    giving index

--- learning_python/solution9.py
+++ learning_python/solution9.py
@@ -1,9 +1,13 @@
 #Remove duplicates from list
-numbers = [99, 99, 0, 0,101, 299, 499, 499]
+# numbers = [99, 99, 0, 0,101, 299, 499, 499]
 
-duplicates = []
+# duplicates = []
 
-for num in numbers:
-    if num not in duplicates:
-     duplicates   .append(num)
-print(duplicates)
+# for num in numbers:
+#     if num not in duplicates:
+#      duplicates   .append(num)
+# print(duplicates)
+
+numbers = [10, 20, 10,30 , 50]
+for i in range(len(numbers)):
+    print(i, numbers[i])
