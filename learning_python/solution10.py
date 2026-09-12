c23a00f1d6db5cc75f04bfe107db7649158aaf63
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-13 00:21:52 +0530

    high values

--- learning_python/solution10.py
+++ learning_python/solution10.py
@@ -1,8 +1,19 @@
 #FIBONACCI SERIES
-n = int(input("Enter number of terms :"))
+# n = int(input("Enter number of terms :"))
 
-a = 0
-b = 1
+# a = 0
+# b = 1
+
+# for i in range(n):
+#     print(a, end=" ")
+#     a, b = b, a + b
+
+
+#practice higher values
+n = int(input("Enter a number series: "))
+
+a = 10
+b = 20
 
 for i in range(n):
     print(a, end=" ")
