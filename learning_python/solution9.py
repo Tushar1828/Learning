0de33fed0a7c629357840261edc356a2569aa02a
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-18 22:20:14 +0530

    add sentence for vowel and consonants

--- learning_python/solution9.py
+++ learning_python/solution9.py
@@ -83,18 +83,36 @@
 
 # Count Vowels and Consonants
 
-text = "python programming"
+# text = "python programming"
+
+# vowels = 0
+# consonants = 0
+
+# for ch in text.lower():
+
+#     if ch in "aeiou":
+#         vowels += 1
+
+#     elif ch.isalpha():
+#         consonants += 1
+
+# print("Vowels:", vowels)
+# print("Consonants:", consonants)
+
+# Q. find vowels and consonants in a sentence by its own 
+
+text = input("Enter a string: ")
 
 vowels = 0
 consonants = 0
 
 for ch in text.lower():
 
-    if ch in "aeiou":
+    if ch in "aieou":
         vowels += 1
 
     elif ch.isalpha():
         consonants += 1
 
-print("Vowels:", vowels)
-print("Consonants:", consonants)
+print("vowels:", vowels)
+print("consonants:", consonants)
