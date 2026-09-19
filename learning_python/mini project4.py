0f5a2858d650eab5baf949d5f5f326d60db76f6e
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-19 19:10:16 +0530

    1st element = while loop

---
+++ learning_python/mini project4.py
@@ -0,0 +1,12 @@
+# BILLING SYSTEM
+
+products = []
+
+while True:
+    print("\n ==== BILING SYSTEM ====")
+    print("1. Add product")
+    print("2. View Bill")
+    print("3. calculate Total")
+    print("4. Exit")
+
+    choice =  input("ENter your choice: ")
