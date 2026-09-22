45bd8c149cbf5bb61c800ced41c90ced71382e9d
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-22 21:29:47 +0530

    enumerate function

--- learning_python/ToughQuestionSeries.py
+++ learning_python/ToughQuestionSeries.py
@@ -22,14 +22,23 @@
 
 # Maximum subarray sum
 
-numbers = [-2, 1, -3, 4, -1, 2, -4, 5, 3]
-current = numbers[0]
-maximum = numbers[0]
+# numbers = [-2, 1, -3, 4, -1, 2, -4, 5, 3]
+# current = numbers[0]
+# maximum = numbers[0]
 
-for i in range(1, len(numbers)):
+# for i in range(1, len(numbers)):
 
-    current = max (numbers [i], current + numbers[i])
+#     current = max (numbers [i], current + numbers[i])
 
-    maximum = max(maximum, current)
+#     maximum = max(maximum, current)
 
-print(maximum)
+# print(maximum)
+
+#WAP TO PRINT THIRD,FIFTH AND SEVENTH ELEMENT FROM A LIST
+#USING ENUMERATE FUNCTION.
+
+l = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for i, item in enumerate(l):
+     if i== 2 or i == 5 or i == 7:
+         print(item)
