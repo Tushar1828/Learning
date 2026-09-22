1b2d060ab96a99612f0ad56698c7016c3d81d8f4
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-22 21:20:35 +0530

    few problem

--- learning_python/ToughQuestionSeries.py
+++ learning_python/ToughQuestionSeries.py
@@ -19,3 +19,17 @@
 #     longest = max(longest,length)
 
 # print(longest)
+
+# Maximum subarray sum
+
+numbers = [-2, 1, -3, 4, -1, 2, -4, 5, 3]
+current = numbers[0]
+maximum = numbers[0]
+
+for i in range(1, len(numbers)):
+
+    current = max (numbers [i], current + numbers[i])
+
+    maximum = max(maximum, current)
+
+print(maximum)
