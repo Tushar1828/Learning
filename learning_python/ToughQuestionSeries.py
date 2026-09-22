2f89c0d48214bb0bb73049c70b997f91fbd45833
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-22 21:18:23 +0530

    maximum subarray sum

--- learning_python/ToughQuestionSeries.py
+++ learning_python/ToughQuestionSeries.py
@@ -1,21 +1,21 @@
 # The longest consecutive sequence
 
-numbers = [100, 4, 500, 29, 85, 1, 3, 2]
+# numbers = [100, 4, 500, 29, 85, 1, 3, 2]
 
-num_set = set(numbers)
-longest = 0
+# num_set = set(numbers)
+# longest = 0
 
-for num in num_set:
+# for num in num_set:
 
-    if num - 1 not in num_set:
+#     if num - 1 not in num_set:
 
-        current = num
-        length = 1
+#         current = num
+#         length = 1
 
-    while current + 1 in num_set:
-        current += 1
-        length +=1
+#     while current + 1 in num_set:
+#         current += 1
+#         length +=1
 
-    longest = max(longest,length)
+#     longest = max(longest,length)
 
-print(longest)
+# print(longest)
