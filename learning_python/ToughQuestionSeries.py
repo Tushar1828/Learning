c56185313d621b0ea2682ce0c6ca6f83e5de887a
Tushar1828 <tushar.sharma0128@gmail.com>
2026-09-23 21:19:26 +0530

    used (IF) statement for output

--- learning_python/ToughQuestionSeries.py
+++ learning_python/ToughQuestionSeries.py
@@ -55,4 +55,10 @@ for i in range(len(text)):
     for j in range(i + 1, len(text) + 1):
 
         substring = text[i:j]
-        
+
+        if substring == substring[::-1]:
+
+            if len(substring) > len(longest):
+                longest = substring
+
+print(longest)
